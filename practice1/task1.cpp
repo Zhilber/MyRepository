eddc460bbd8d4592463d1a5b79f2dc4ecb06f4d4
Zhilber <yaroslavbrich@gmail.com>
2024-09-28 09:54:37 +0300

    Upgraded questions 1 and 4

--- practice1/task1.cpp
+++ practice1/task1.cpp
@@ -5,14 +5,34 @@ using namespace std;
 int main() {
     double a, b;
 
-    cout << "Enter first side: ";
+    cout << "Enter length: ";
     cin >> a;
 
-    cout << "Enter second side: ";
+    cout << "Enter width: ";
     cin >> b;
 
-    cout << "s = "<< a * b << endl;
-    cout << "p = " << 2 * (a + b);
+    double amount1 = a * b;
+    double amount2 = 2 * (a + b);
 
-    cout << endl << &a << endl << &b;
+    cout << "s = "<< amount1 << endl;
+    cout << "p = " << amount2 << endl << endl;
+
+    double sum = amount1 + amount2;
+    double difference = amount1 - amount2;
+    double dlnna = amount1 / amount2;
+
+    cout << "Amount (+): " << sum << endl;
+    cout << "difference (-): " << difference << endl;
+    cout << "dilation (/):" << dlnna << endl << endl;
+
+    cout << "Address of a: " << &a << endl;
+    cout << "Address of amount1: " << &amount1 << endl;
+    cout << "Address of amount2: " << &amount2 << endl;
+    cout << "Address of sum: " << &sum << endl;
+    cout << "Address of difference: " << &difference << endl;
+    cout << "Address of dlnna: " << &dlnna << endl;
+
+
+    return 0;
+
 }
--- practice1/task4.cpp
+++ practice1/task4.cpp
@@ -10,7 +10,6 @@ int main() {
     if (age >= 20) {
         cout << "Enter your salary: ";
         cin >> salary;
-
     }
     else {
         cout << "You are not suitable" << endl;
@@ -18,11 +17,27 @@ int main() {
     }
 
     if (salary >= 400 && salary <= 1000) {
-        cout << "We will consider your position" << endl;
+        cout << "We will consider your position" << endl << endl;
     }
     else {
-        cout << "You are not suitable" << endl;
-    return -1;
+        cout << "You are not suitable" << endl << endl;
+
+        return -1;
     }
-    cout << "Your age: " << age <<endl <<  "Your salary: " << salary;
+    string ft_and_lt_name;
+
+    cout << "Enter your first and last name: ";
+    cin >> ft_and_lt_name;
+
+    string position;
+
+    cout << "Enter position: ";
+    cin >> position;
+
+    //Виведення інформації
+
+    cout << endl << "Your name: " << ft_and_lt_name << endl;
+    cout << "Your age: " << age << endl;
+    cout << "Your position: " << position << endl;
+    cout << "Your selary" << salary << endl;
 }
